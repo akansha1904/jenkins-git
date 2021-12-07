568bdd0e727be941e954330e13f63a493cf456c1
aka <akanshaimmanuel19@gmail.com>
2021-12-07 21:17:39 +0530

    initail commit

--- kube-pods.sh
+++ kube-pods.sh
@@ -5,8 +5,8 @@ echo "list pods"
 
 cat "$CREDENTIALS_ID" > key.json
 docker login -u _json_key -p "$(cat key.json)" https://gcr.io
-$GCLOUD_PATH/gcloud container clusters get-credentials tomcat-service --zone us-central1-c
-$GCLOUD_PATH/gcloud container images list --repository=gcr.io/tomcat-70998
+gcloud container clusters get-credentials tomcat-service --zone us-central1-c
+gcloud container images list --repository=gcr.io/tomcat-70998
 
 
 kubectl get pods
