7336a0ea860a278676845c26634a7f32f36524bf
aka <akanshaimmanuel19@gmail.com>
2021-12-07 20:58:16 +0530

    initial commit

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ echo " Deploying to cluster "
 
 cat "$CREDENTIALS_ID" > key.json
 docker login -u _json_key -p "$(cat key.json)" https://gcr.io
-$GCLOUD_PATH/gcloud container clusters get-credentials tomcat-service --zone us-central1-c
+sudo $GCLOUD_PATH/gcloud container clusters get-credentials tomcat-service --zone us-central1-c
 kubectl create -f deployment.yml
  
 docker logout https://gcr.io
