e39effa49f2aa8c234d233e4222b73c7f9884660
aka <akanshaimmanuel19@gmail.com>
2021-12-09 16:45:57 +0530

    commit

--- gcr-push.sh
+++ gcr-push.sh
@@ -5,7 +5,7 @@ echo "image push"
 docker tag tomcat-70998/jenkins-assignment:v3 gcr.io/tomcat-70998/jenkins-assignment:v3
 cat "$CREDENTIALS_ID" > key.json
 docker login -u _json_key -p "$(cat key.json)" https://gcr.io
-docker push gcr.io/tomcat-70998/jenkins-assignment:v2
+docker push gcr.io/tomcat-70998/jenkins-assignment:v3
 docker logout https://gcr.io
      
 
