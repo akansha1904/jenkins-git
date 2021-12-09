9eb7fa6a8c92c58865788f4f352ae0fe39009fff
aka <akanshaimmanuel19@gmail.com>
2021-12-09 16:31:37 +0530

    commit

--- kube-pods.sh
+++ kube-pods.sh
@@ -5,7 +5,9 @@ echo "list pods"
 
 cat "$CREDENTIALS_ID" > key.json
 docker login -u _json_key -p "$(cat key.json)" https://gcr.io
-gcloud container clusters get-credentials tomcat-service --zone us-central1-c
+ gcloud auth activate-service-account --key-file=${CREDENTIALS_ID}
+gcloud container clusters get-credentials tomcat-service --zone us-central1-c --project tomcat-70998
+
 gcloud container images list --repository=gcr.io/tomcat-70998
 
 
