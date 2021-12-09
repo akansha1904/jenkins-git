7f85d93cdc3d65419919d088d90dd0203f0b5578
aka <akanshaimmanuel19@gmail.com>
2021-12-09 16:34:53 +0530

    commit

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ echo "deploy stage";
 docker login -u _json_key -p "$(cat key.json)" https://gcr.io
    gcloud auth activate-service-account --key-file=${CREDENTIALS_ID}
 gcloud container clusters get-credentials tomcat-service --zone us-central1-c --project tomcat-70998
-kubectl create -f deployment.yml
+kubectl create -f deployment.yaml
  
 docker logout https://gcr.io
 
